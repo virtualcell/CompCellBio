d1eba6df30cd150c52d6a21cbf24df5d1727abe7
GMarupilla <marupilla@mail.com>
2020-12-18 15:56:01 -0500

    Routing working from nav for different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { DatabaseResourcesComponent } from './components/database-resources/database-resources.component';
+import { EducationalResourcesComponent } from './components/educational-resources/educational-resources.component';
+import { HomeComponent } from './components/home/home.component';
+import { PublicationsComponent } from './components/publications/publications.component';
+import { SoftwareResourcesComponent } from './components/software-resources/software-resources.component';
+import { UpcomingWorkshopsComponent } from './components/upcoming-workshops/upcoming-workshops.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'educational-resources', component: EducationalResourcesComponent },
+  { path: 'database-resources', component: DatabaseResourcesComponent },
+  { path: 'software-resources', component: SoftwareResourcesComponent },
+  { path: 'publications', component: PublicationsComponent },
+  { path: 'upcoming-workshops', component: UpcomingWorkshopsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
