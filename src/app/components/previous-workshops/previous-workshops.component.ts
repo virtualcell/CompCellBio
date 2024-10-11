71b84f7ce3a63b6511e001507fe4f2d81f6b93da
smstaurovsky <75797434+smstaurovsky@users.noreply.github.com>
2024-10-11 16:00:25 -0400

    Create previous-workshops.component.ts

---
+++ src/app/components/previous-workshops/previous-workshops.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-workshops',
+  templateUrl: './previous-workshops.component.html',
+  styleUrls: ['./previous-workshops.component.scss']
+})
+export class PreviousWorkshopsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
